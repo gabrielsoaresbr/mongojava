3b79c7fe37182561b2ce77c85216e5fa5bd6aded
gabriel.soares <gabriel.soares@dell024831.ans.gov.br>
2017-07-19 19:01:33 -0300

    Criação do projeto

---
+++ mongojava/src/main/java/br/com/gabriel/mongojava/MongojavaApplication.java
@@ -0,0 +1,12 @@
+package br.com.gabriel.mongojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongojavaApplication.class, args);
+	}
+}
